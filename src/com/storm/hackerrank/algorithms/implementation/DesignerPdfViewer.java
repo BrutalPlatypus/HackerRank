13ec6a83efbb9c176a4265e1ae388ffd2a435dd4
BrutalPlatypus <viper_king75@yahoo.com>
2016-12-02 23:22:03 -0500

    Initial commit got screwed again

--- src/com/storm/hackerrank/algorithms/implementation/DesignerPdfViewer.java
+++ src/com/storm/hackerrank/algorithms/implementation/DesignerPdfViewer.java
@@ -1,4 +1,21 @@
 package com.storm.hackerrank.algorithms.implementation;
 
+import java.util.Scanner;
+
 public class DesignerPdfViewer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] letterHeights = new int[26];
+        for (int i = 0; i < 26; i++) {
+            letterHeights[i] = in.nextInt();
+        }
+        String word = in.next();
+        int maxHeight = 0;
+        for (char c : word.toCharArray()) {
+            int height = letterHeights[(int)c - (int)'a'];
+            maxHeight = Math.max(maxHeight, height);
+        }
+        System.out.println(maxHeight * word.length());
+    }
 }
--- src/com/storm/hackerrank/algorithms/implementation/ViralAdvertising.java
+++ src/com/storm/hackerrank/algorithms/implementation/ViralAdvertising.java
@@ -1,8 +1,19 @@
 package com.storm.hackerrank.algorithms.implementation;
 
+import java.util.Scanner;
+
 public class ViralAdvertising {
 
     public static void main(String[] args) {
-        //TODO
+        Scanner in = new Scanner(System.in);
+        int days = in.nextInt();
+        int totalLikeCount = 0;
+        int people = 5;
+        for (int i = 0; i < days; i++) {
+            people = people / 2;
+            totalLikeCount += people;
+            people *= 3;
+        }
+        System.out.println(totalLikeCount);
     }
 }
